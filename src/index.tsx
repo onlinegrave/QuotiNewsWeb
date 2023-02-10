1c1b9c7b12960e7bc7431d35434ad2ea324513b1
dambar <dambar.p@mapking.com>
2023-02-11 00:39:37 +0800

    'deploy'

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import AppRouter from './navigation/_routes';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApq02EYpr7xL3TNiv00ErILI54Z2iERYU",
+  authDomain: "quotinews.firebaseapp.com",
+  projectId: "quotinews",
+  storageBucket: "quotinews.appspot.com",
+  messagingSenderId: "474943970168",
+  appId: "1:474943970168:web:5f0ee3d55681147cd4c68d",
+  measurementId: "G-WGGXPMZGF9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
